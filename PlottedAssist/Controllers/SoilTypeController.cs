09a13e97dfd94eaae7a503d6f4ea501d212bc5a7
amol343 <aghu0002@student.monash.edu>
2020-05-21 20:41:57 +1000

    Added Static Pages

---
+++ PlottedAssist/Controllers/SoilTypeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlottedAssist.Controllers
+{
+    public class SoilTypeController : Controller
+    {
+        // GET: SoilType
+        public ActionResult SoilTypes()
+        {
+            return View();
+        }
+
+        // GET: WateringGuide
+        public ActionResult WateringGuide()
+        {
+            return View();
+        }
+
+        // GET: WildlifeTips
+        public ActionResult WildlifeTips()
+        {
+            return View();
+        }
+
+        // GET: PlantTech
+        public ActionResult PlantTech()
+        {
+            return View();
+        }
+
+        // GET: PlantTech
+        public ActionResult Prune()
+        {
+            return View();
+        }
+        public ActionResult OtherCare()
+        {
+            return View();
+        }
+    }
+}
